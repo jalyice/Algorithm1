935878cbfcd276b83aa0448632207e3652ca3e1c
jalyice <59529944+jalyice@users.noreply.github.com>
2023-07-11 17:26:21 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 136 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=28;
+        boolean[] arr = new boolean[30];
+        while(n-- > 0){
+            arr[Integer.parseInt(br.readLine())-1]=true;
+        }
+        for(int i=0; i<30; i++){
+            if(arr[i]==false) bw.write(i+1+"\n");
+        }
+        bw.close();    
+        
+    }
+}

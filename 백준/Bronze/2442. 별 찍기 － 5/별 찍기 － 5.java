2f0323faf65d530040813949757af13912ead7ad
jalyice <59529944+jalyice@users.noreply.github.com>
2023-07-14 20:29:35 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 132 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=N; i++){
+            
+            for(int j=i; j<N; j++){
+                bw.write(" ");
+            }
+            for(int k=0; k<i*2-1; k++){
+                bw.write("*");
+            }
+            
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}

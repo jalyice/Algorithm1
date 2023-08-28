0f3ce5bac8b9f311310c4150cde813d66b924fe4
jalyice <59529944+jalyice@users.noreply.github.com>
2023-08-28 17:23:43 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,11 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        bw.write(recur(N,0)+"\n");
-        bw.close();
+        System.out.println(recur(N,0));
+    
     }
 
     static int recur(int N, int count){

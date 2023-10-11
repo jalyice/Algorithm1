72c70ef3cf7c26d019eda3e7aabd934d83de25e9
jalyice <59529944+jalyice@users.noreply.github.com>
2023-10-11 16:04:39 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int T, x, y;
+
+    cin >> T;
+    for(int i=0; i<T; i++){
+        cin >> x >> y;
+        cout << "Case #" << i+1 << ": " << x+y << endl;
+    }
+    
+    return 0;
+}

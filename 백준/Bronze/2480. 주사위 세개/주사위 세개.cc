117a277c5efaa641c006f644b1c70179ce8f2250
jalyice <59529944+jalyice@users.noreply.github.com>
2023-10-11 15:40:07 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -6,9 +6,8 @@ int main()
     int x, y, z;
     cin >> x >> y >> z;
     if( x == y && y == z ) cout << 10000 + x*1000 ; 
-    else if ( x == y && y!=z && x!=z )cout << 1000 + x*100;
-    else if(y == z && x!=y && x!= z) cout << 1000 + y*100;
-    else if(x==z && x!=y && y!=z) cout<< 1000 + z*100;
+    else if ( x == y || x==z )cout << 1000 + x*100;
+    else if(y == z ) cout << 1000 + y*100;
     else {
         if(x > y && x > z) cout << x*100;
         else if( y > x && y > z ) cout << y*100;

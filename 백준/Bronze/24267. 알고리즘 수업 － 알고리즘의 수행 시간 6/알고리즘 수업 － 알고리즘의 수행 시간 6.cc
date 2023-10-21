0e700e55a4d11bf6e5a8f31035303d87e390e709
jalyice <59529944+jalyice@users.noreply.github.com>
2023-10-21 15:09:47 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    long int n;
+    cin >> n;
+    cout << n*(n-1)*(n-2)/6 << endl << 3 << endl;
+
+
+    return 0;
+}

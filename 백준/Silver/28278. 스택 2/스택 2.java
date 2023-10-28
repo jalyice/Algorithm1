7503e2a7fe139a0773a89322e2d46b9e8dd6b1db
jalyice <59529944+jalyice@users.noreply.github.com>
2023-10-28 15:30:56 +0900

    [Silver IV] Title: 스택 2, Time: 1084 ms, Memory: 213664 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] stack = new int [1000000];
+        int idx = 0;
+        while(N -- > 0){
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            switch(k){
+                case 1:		
+		        	int num = Integer.parseInt(st.nextToken());
+                    stack[idx++] = num;
+		        	break;
+        		case 2:		
+                    if(idx == 0) bw.write(-1+"\n");//System.out.println(-1);
+                    else {
+                        bw.write(stack[idx-1]+"\n");
+                        //System.out.println(stack[idx-1]);    
+                        stack[idx--] = 0;
+                    }
+			        break; 
+		        case 3:
+                    bw.write(idx+"\n");
+			        //System.out.println(idx);
+			        break;
+                case 4:
+                    if(idx==0) bw.write(1+"\n");//System.out.println(1);
+                    else bw.write(0+"\n");//System.out.println(0);
+                    break;
+                case 5:
+                    if(idx==0) bw.write(-1+"\n");//System.out.println(-1);
+                    else bw.write(stack[idx-1]+"\n");//System.out.println(stack[idx-1]);
+                    break;
+	        	default:
+                    break;
+            }
+            
+        }
+        bw.close();
+
+        
+       
+    }
+}
